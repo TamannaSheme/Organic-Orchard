72dd337f87af995032e429011d76dca43e42338d
TamannaSheme <syedatsheme03@gmail.com>
2023-01-02 15:01:10 +0600

    Admin Section Added

--- admin_contacts.php
+++ admin_contacts.php
@@ -45,7 +45,7 @@ if(isset($_GET['delete'])){
 
     <section class="messages">
 
-        <h1 class="title">messages</h1>
+        <h1 class="title">Messages</h1>
 
         <div class="box-container">
 
@@ -56,18 +56,18 @@ if(isset($_GET['delete'])){
          while($fetch_message = $select_message->fetch(PDO::FETCH_ASSOC)){
    ?>
             <div class="box">
-                <p> user id : <span><?= $fetch_message['user_id']; ?></span> </p>
-                <p> name : <span><?= $fetch_message['name']; ?></span> </p>
-                <p> number : <span><?= $fetch_message['number']; ?></span> </p>
-                <p> email : <span><?= $fetch_message['email']; ?></span> </p>
-                <p> message : <span><?= $fetch_message['message']; ?></span> </p>
+                <p> User ID : <span><?= $fetch_message['user_id']; ?></span> </p>
+                <p> Name : <span><?= $fetch_message['name']; ?></span> </p>
+                <p> Number : <span><?= $fetch_message['number']; ?></span> </p>
+                <p> Email : <span><?= $fetch_message['email']; ?></span> </p>
+                <p> Message : <span><?= $fetch_message['message']; ?></span> </p>
                 <a href="admin_contacts.php?delete=<?= $fetch_message['id']; ?>"
-                    onclick="return confirm('delete this message?');" class="delete-btn">delete</a>
+                    onclick="return confirm('Delete this Message?');" class="delete-btn">Delete</a>
             </div>
             <?php
          }
       }else{
-         echo '<p class="empty">you have no messages!</p>';
+         echo '<p class="empty">You have no Messages!</p>';
       }
    ?>
 
