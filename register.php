b224241cc5103bd27f5caf4ece561d6a3c834180
TamannaSheme <syedatsheme03@gmail.com>
2023-01-03 21:54:05 +0600

    Done

--- register.php
+++ register.php
@@ -85,14 +85,14 @@ if(isset($message)){
     <section class="form-container">
 
         <form action="" enctype="multipart/form-data" method="POST">
-            <h3>register now</h3>
+            <h3>Register Now</h3>
             <input type="text" name="name" class="box" placeholder="enter your name" required>
             <input type="email" name="email" class="box" placeholder="enter your email" required>
             <input type="password" name="pass" class="box" placeholder="enter your password" required>
             <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
             <input type="file" name="image" class="box" required accept="image/jpg, image/jpeg, image/png">
             <input type="submit" value="register now" class="btn" name="submit">
-            <p>already have an account? <a href="login.php">login now</a></p>
+            <p>Already have an Account? <a href="login.php">Login Now</a></p>
         </form>
 
     </section>
